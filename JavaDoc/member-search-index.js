15dfe5fa920c7701ccc772ba0659ba6327bdfa51
AidulFikri <muhammadaidul112@gmail.com>
2023-11-01 22:01:25 +0700

    progamer

---
+++ JavaDoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"PalindromeChecker","l":"isPalindrome(String)","u":"isPalindrome(java.lang.String)"},{"p":"<Unnamed>","c":"PalindromeChecker","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"PalindromeChecker","l":"PalindromeChecker()","u":"%3Cinit%3E()"}];updateSearchResults();
